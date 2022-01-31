ecae6b2172118a4b7ba35052de19812bcf1a409d
Tony Chacon <softwareintrospectre@gmail.com>
2022-01-31 14:01:42 -0500

    Update main.py
    * Dataframes now combine into one Output Dataframe (single result set)
    * Successfully load to MS SQL Server table

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 '''
-    What I learned:
-        - Have a robust yet agile plan in place first
-        - Research how often the API is maintained, to prevent getting stuck halfway through the project
-            - If there's lots of bugs on the forums, then maybe don't go with that one.
-'''
-'''
-    Plan:
+    Intention: 
+    
+    1. To use my Spotify credentials to connect to Spotify API using the Client Credentials Flow (Authorization: OAuth2) 
 
-    def process_data()
-        1. Define and import ENVIRONMENT_VARIABLES to access my Spotify account
-        2. Encode CLIENT_ID and CLIENT_SECRET into an encoded byte string, then decode into string (for authorization header)
-        3. Define the API endpoint (specific song data I'm looking for)
-        4. Define headers and data dictionaries, to contain headers and data lists
-        5. Define grant type as "client_credentials", because I only care about server connection (no app/client required here)
-        6. Make an HTTP POST request to spotify, requesting the OAuth Token (use the provided Token URL)
-        7. Store the response JSON object into a variable
-        8. 
+    2. Obtain my Recently Played Songs Data (equivalent to: song name, musician name, played at (datetime), datetime_timestamp) from the past 24 hours
+
+    3. Transform the imported data into a tabular format so that it can be export to Microsoft SQL Server
+
+    4. Import data into Microsoft SQL Server --> KATSUKI Server --> [API_Testing].[dbo].[Spotify_Data_Load] table
+
+    5. Upon import: Generate a primary key for each record (Song_Played_SKey), using the played_at_datetime field
+        Why: Only 1 song can be played at any given moment in time, making it a good candidate
+
+    6. Have the process automatically occur each day
+        6A. Create an SSIS package with the "Execute Script" task
+
+    X. If there's no songs played within past 24 hours, have that be an acceptable case (create a log file for this.)
+            *consider what type of logging information would be useful
+                * Log each major step. If failure, log exact failure. 
+                * If no records for that day, log that case
+
+Why:
+    1. Demonstrate ability to grab data via an API via Python
+    2. Ability to handle edge cases in script
+    3. Demonstrate logging
+    4. Experience creating an SSIS package with Python script
+    5. Experience maintaining a regularly recurring ETL independently
+    6. Experience creating and maintaining data in Dimension Table
+
+Example:
+    Song_Name	Musician_Name	Played_At_Datetime	        Date_Timestamp	        Song_Played_SKey (PK)
+    The Castle	Nobuo Uematsu	2021-12-08T16:12:57.797Z	2021-12-08	            1
 '''
 
 # used to access environment variables
@@ -39,49 +54,24 @@ import urllib
 # create database enging connection
 from sqlalchemy import create_engine
 
-import spotipy
-
+# used to format JSON data
 import json
 
-# # validation checks
-# def check_if_valid_data(df: pd.DataFrame) -> bool:
+# def generate_log_table_dataframe():
+#     '''
+#         Collect logging data, convert to dataframe, push to table
+#     '''
+#     return None
 
-#     print(df)
+# def generate_main_table_dataframe():
+#     '''
+#         Take API data, convert to dataframe, push to table
+#     '''
+#     return None
 
-#     #check if dataframe is empty
-#     if df.empty:
-#         print("No songs played.")
-#         return False
+def load_dataframe_to_database(dataframe_for_sql, server_name, database_name, schema_name, table_name):
 
-#     # Primary Key (PK) check: played at is PK because it's a unique timestamp (enforces Primary Key Constraint)
-#     if pd.Series(df['Played_At_Datetime']).is_unique:
-#         pass
-
-#     else:
-#         raise Exception("Primary Key check is violated.")
-
-#     # Null values check
-#     if df.isnull().values.any():
-#         raise Exception("Null values found.")
-
-def generate_log_table_dataframe():
-    '''
-        Collect logging data, convert to dataframe, push to table
-    '''
-    return None
-
-def generate_main_table_dataframe():
-    '''
-        Take API data, convert to dataframe, push to table
-    '''
-    return None
-
-def load_dataframe_to_database(dataframe_for_sql, server_name, database_name):
-    '''
-        load dataframe to database
-    '''
-    
-    quoted = urllib.parse.quote_plus("Driver={SQL Server};"
+    quoted = urllib.parse.quote_plus("Driver=SQL Server;"
                                     "Server=" + server_name + ";" +
                                     "Database=" + database_name +";")
 
@@ -90,7 +80,7 @@ def load_dataframe_to_database(dataframe_for_sql, server_name, database_name):
     connection_engine = create_engine('mssql+pyodbc:///?odbc_connect={}'.format(quoted))
     print('Database connection established.')
 
-    dataframe_for_sql.to_sql('Spotify_Data_Load', schema='dbo', con=connection_engine, if_exists='append')
+    dataframe_for_sql.to_sql(table_name, schema=schema_name, con=connection_engine, if_exists='append', index=False)#, dtype=dict)
     print('table populated.')
 
     return True
@@ -99,13 +89,14 @@ def load_dataframe_to_database(dataframe_for_sql, server_name, database_name):
 def process_data():
         #https://developer.spotify.com/console/get-recently-played/?limit=10&after=1636229680000&before=
     '''
-        API Reference: Get Recently Played Tracks
-        Endpoint: https://api.spotify.com/v1/me/player/recently-played
+        API Reference: Get New Releases
+        Endpoint: https://api.spotify.com/v1/browse/new-releases
         HTTP Method: GET
         OAuth: Required
+        https://developer.spotify.com/console/get-new-releases/?country=US&limit=10&offset=5
 
-        GET https://api.spotify.com/v1/me/player/recently-played
-        limit = 50 (maximum)
+        limit = 10  (maximum)
+        offset = 5
         after 1636229680000 (yesterday_unix_timestamp)
 
         OAuth Token: GetToken --> Required scopes for this endpoint: user-read-recently-played
@@ -125,85 +116,48 @@ def process_data():
     CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET_ENV')
     BASE64_ENCODED_HEADER_STRING = base64.b64encode(bytes(f"{CLIENT_ID}:{CLIENT_SECRET}", "ISO-8859-1")).decode("ascii")
     #must be between 1 and 50
-    song_limit_amount= 2
+    song_limit_amount= 10
     song_limit_amount_string = str(song_limit_amount)
-
-    offset_amount = 5
-    offset_amount_string = str(offset_amount)
-
-    print("Song limit amount: ", song_limit_amount_string)
 
     print('Limit: ' + song_limit_amount_string)
 
     token_request_url = "https://accounts.spotify.com/api/token"
+    headers = {}
+    data = {}
 
-    headers_dict = {}
-    data_dict = {}
+    #api_call_url = f"https://api.spotify.com/v1/me/player/recently-played?limit={song_limit_amount_string}&after={yesterday_unix_timestamp_string}"
+    api_endpoint_url  = "https://api.spotify.com/v1/browse/new-releases?country=US&limit=10&offset=5"
+    print("api_call_url: ", api_endpoint_url)
 
-    # HTTP GET request returned Status 500, so bug on Spotify's end. Pivoting to other data (1/19/2022)
-    # get_request_api_endpoint = f"https://api.spotify.com/v1/me/player/recently-played?limit={song_limit_amount_string}&after={yesterday_unix_timestamp_string}"
-    get_request_api_endpoint = f"https://api.spotify.com/v1/users/gw4ojoz76rzzmzy1gchctig37/playlists?offset={offset_amount_string}&limit={song_limit_amount_string}"
-
-
-    print("get_request_api_endpoint: ", get_request_api_endpoint)
-
-    headers_dict['Authorization'] = f"Basic {BASE64_ENCODED_HEADER_STRING}"
-    data_dict['grant_type'] = "client_credentials"
-    data_dict['json'] = True
+    headers['Authorization'] = f"Basic {BASE64_ENCODED_HEADER_STRING}"
+    data['grant_type'] = "client_credentials"
+    data['json'] = True
 
     #multiple scopes requrire a space in the same string
-    # data_dict['scope'] = 'user-read-recently-played'
-    data_dict['scope'] = 'playlist-read-private'
+    #data['scope'] = 'user-top-read'
 
     # make API request to request OAuth Token
-    r = requests.post(token_request_url, data=data_dict, headers=headers_dict)
-
-    token = r.json()['access_token']
-    print("Token: " + token)
+    r = requests.post(token_request_url, headers=headers, data=data)
 
     # prints the response from the server regarding the access token data
-    print(json.dumps(r.json(), indent=2))    
+    print(json.dumps(r.json(), indent=2))
+    
+    token = r.json()['access_token']
 
     # use token to access data
-    get_request_headers = {
+    headers = {
         "Accept": "application/json",
-        "Content-Type": 'application/json', #"application/json",
+        "Content-Type": "application/json",
         "Authorization": "Bearer " + token
     }
 
+    res = requests.get(url=api_endpoint_url, headers=headers)
 
-    # Need to fix how I use the Access Token
-
-
-    res = requests.get(url=get_request_api_endpoint, headers=get_request_headers)
-    
-    # recently_played_songs_object = (json.dumps(res.json(), indent=2))
-    
-    data = (json.dumps(res.json(), indent=2))
+    print(res)    
+    #data = json.dumps(res.json(), indent=2)
+    data = res.json()
 
     print(data)
-
-    '''
-    Thursday 1/20/2022 TODO:
-        - Proved that "Recently Played Tracks" endpoint is buggy (Code: 500 - Server Error)
-        - Experimented with the "Get User Playlists" endpoint, proved that endpoint is working successfully.
-    '''
-
-    '''
-        Friday 1/21/2022 TODO:
-            1. Investigate Spotify's API endpoints to use
-            2. Think through which endpoints could be useful
-            3. Narrow useful endpoints down to 1
-            4. Rework code to use that endpoint
-            5. Create SSIS Package to call Python script
-            6. Create scheduled job in SSMS
-            7. Set the job to run daily
-            8. Check on it Monday 
-    '''
-
-    # Bug: https://community.spotify.com/t5/Spotify-for-Developers/Bug-with-an-API-Recently-Played-Tracks/td-p/5065969
-
-    #print(json.dumps(res.json(), indent=2))
 
 #curl -X "GET" "https://api.spotify.com/v1/me/player/recently-played?limit=50&after=1636229680000" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQAiTwBR21RFKg-MJM4onyICyeTUH-Nftg4p9a-9m4gFxGjnLIXg5GuZQ4OHy9PxkwlXOM1uL58D5fCkAKfe0Z3hMw79rKolf8BM7F3HQ7NH_s8ZTTYDHd136IxqISvOlJDPvY8weTCmY8ivBahdQVtqDZz77_DCSNgUx4I0"
 
@@ -220,63 +174,131 @@ def process_data():
 # testing for 3 days ago worth of data
     #http_response = requests.get("https://api.spotify.com/v1/me/player/recently-played?limit=10&after={time}".format(time=three_days_ago_unix_timestamp), headers = spotify_API_headers)
 
-    # data = http_response.json()
-
     # print(data)
 
     #print(data)
-'''
-    # the only field names I care about
-    song_names = []
-    artist_names = []
-    song_added_at_playlist = []
-    timestamps = []
 
-    song_dataframe = pd.DataFrame()
+    output_dataframe = pd.DataFrame()
 
-    # print(song_dataframe)
+# the only field names I care about
+    album_ids_list = [] #"id": "0sb3OPjnOZEgWrAhwSyGJc"
+    album_types_list  = [] #"type": "album"
+    album_names_list  = [] #"name": "on to better things",
+    album_release_dates_list  = [] # "release_date": "2022-01-21"
+    album_release_dates_precision_list = [] # "release_date_precision" : "day"
+    album_total_tracks_list  = [] #"total_tracks": 15
+    album_spotify_urls_list  = [] #"external_urls": {"spotify": "https://open.spotify.com/album/0sb3OPjnOZEgWrAhwSyGJc"}
+    album_artist_ids_list  = [] #"id": "6ASri4ePR7RlsvIQgWPJpS"
+    album_artist_names_list  = [] #"name": "iaan dior"
+
+# album_dictionary:  {
+#     'Album_ID': ['0sb3OPjnOZEgWrAhwSyGJc']
+#     , 'Album_Type': ['album']
+#     , 'Album_Name': ['on to better things']
+# , 'Album_Release_Date': ['2022-01-21']
+# , 'Album_Release_Date_Precision': ['day']
+# , 'Album_Total_Tracks': [15]
+# , 'Album_Spotify_URL': ['https://open.spotify.com/album/0sb3OPjnOZEgWrAhwSyGJc']
+# , 'Album_Artist_ID': ['6ASri4ePR7RlsvIQgWPJpS']
+# , 'Album_Artist_Name': ['iann dior']
+# }
+
+    '''
+        "album_type": "album",
+        "artists": [
+            "id": "6ASri4ePR7RlsvIQgWPJpS"
+            "name": "iann dior"
+        ]
+        "id": "0sb3OPjnOZEgWrAhwSyGJc"
+    '''
+
+    # song_dataframe = pd.DataFrame()
 
     # attempt to append data from API to a dictionary, then to a dataframe
-    # try:
 
-        # grabbing elements from data's item list (master results)
-    for song in data["items"]:
+    # grabbing elements from data's item list (master results)
+    for item in data["albums"]["items"]:
+    
+        print("album_id: ", item["id"])
+        album_ids_list.append(item["id"])
 
-        # iterating to inner list: names and adding individual name per song
-        song_names.append(song["track"]["name"])
+        print("album_type: ", item["album_type"])
+        album_types_list.append(item["album_type"])
 
-        # iterating to individual inner inner list: artists and adding individual artist per son
-        artist_names.append(song["track"]["album"]["artists"][0]["name"])
+        print("album_name: ", item["name"])
+        album_names_list.append(item["name"])
 
-        song_added_at_playlist.append(song["added_at"])
+        print("release_date: ", item["release_date"])
+        album_release_dates_list.append(item["release_date"])
 
-        timestamps.append(song["added_at"][0:10])
+        print("release_date_precision: ", item["release_date_precision"])
+        album_release_dates_precision_list.append(item["release_date_precision"])
 
-    song_dictionary = {
-        "Song_Name": song_names,
-        "Musician_Name": artist_names,
-        "Song_Added_At_Datetime": song_added_at_playlist,
-        "Date_Timestamp": timestamps
-    }
+        print("total_tracks: ", item["total_tracks"])
+        album_total_tracks_list.append(item["total_tracks"])
+        
+        print("spotify_url: ", item["external_urls"]["spotify"])
+        album_spotify_urls_list.append(item["external_urls"]["spotify"])
 
-    song_dataframe = pd.DataFrame(data=song_dictionary, index=False, columns=["Song_Name", "Musician_Name", "Played_At_Datetime", "Date_Timestamp"])
+        for artist in item["artists"]:
+            print("artist_id: ", artist["id"])
+            album_artist_ids_list.append(artist["id"])
 
-    print(song_dataframe)
+            print("artist_name: ", artist["name"])
+            album_artist_names_list.append(artist["name"])
 
-    # # Fails if Token is expired
-    # except:
-    #     print("Token expired. Get a new one.")
 
-    #     # used to prepare pandas dataframe (tabular formatted data)
+        album_dictionary = {
+            "Album_ID": album_ids_list,
+            "Album_Type": album_types_list,
+            "Album_Name": album_names_list,
+            "Album_Release_Date": album_release_dates_list,
+            "Album_Release_Date_Precision": album_release_dates_precision_list,
+            "Album_Total_Tracks": album_total_tracks_list,
+            "Album_Spotify_URL": album_spotify_urls_list,
+            # "Album_Artist_ID": album_artist_ids_list,
+            # "Album_Artist_Name": album_artist_names_list
+            }
 
-    # # Validate
-    # if check_if_valid_data(song_dataframe) is not False:
-    #     print("Data valid, proceed to Load stage.")
+        print("album_dictionary: ", album_dictionary)
 
-    #     # Load
-    #     load_dataframe_to_database(song_dataframe, 'KATSUKI', 'API_TESTING')
+        dataframe_columns = {
+            "Album_ID": album_dictionary["Album_ID"],
+            "Album_Type": album_dictionary["Album_Type"],
+            "Album_Name": album_dictionary["Album_Name"],
+            "Album_Release_Date": album_dictionary["Album_Release_Date"],
+            "Album_Release_Date_Precision": album_dictionary["Album_Release_Date_Precision"],
+            "Album_Total_Tracks": album_dictionary["Album_Total_Tracks"],
+            "Album_Spotify_URL": album_dictionary["Album_Spotify_URL"],
+            # "Album_Artist_ID": album_dictionary["Album_Artist_ID"],   #TODO: Fix the One to Many problem (multiple artists per album potentially). Maybe use a Lambda Function here?
+            # "Album_Artist_Name": album_dictionary["Album_Artist_Name"] #TODO: Fix the One to Many problem (multiple artists per album potentially). Maybe use a Lambda Function here?
+        }
 
-    # else:
-    #     print("Nothing to load. Exiting.")
-'''
+        album_dataframe = pd.DataFrame(dataframe_columns) 
+        print("Album Dataframe: ",album_dataframe)
+        
+
+        # concatenates each album's dataframe to the output dataframe
+        print("Appending current dataframe to Output Dataframe")
+        output_dataframe = output_dataframe.append(album_dataframe)
+
+        print(output_dataframe)
+    #     return
+
+        output_dataframe = output_dataframe.drop_duplicates()
+
+        print(output_dataframe)
+
+    print('Finished iterating through JSON data. Moving to SQL Load.')
+    load_dataframe_to_database(output_dataframe, 'KATSUKI', 'API_TESTING', 'dbo', 'Spotify_Top_Ten_New_Releases') 
+    
+    #TODO: Fix the issue loading this into SQL: "(pyodbc.Programming Error"
+        # Exception has occurred: ProgrammingError
+        # (pyodbc.ProgrammingError) ('42000', "[42000] [Microsoft][ODBC SQL Server Driver][SQL Server]Incorrect syntax near ')'. (102) (SQLExecDirectW)")
+        # [SQL: 
+        # CREATE TABLE dbo.[Spotify_Top_Ten_New_Releases] (
+        # )
+
+        # ]
+
 process_data()
